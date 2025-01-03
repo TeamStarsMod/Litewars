b24e9725ea48178515d630f99b1cc99bbc2445d9
Neko110923 <neko110923@163.com>
2025-01-03 23:03:51 +0800

    欢迎来到Litewars! (首次提交)

---
+++ src/main/java/xyz/litewars/litewars/Litewars.java
@@ -0,0 +1,16 @@
+package xyz.litewars.litewars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Litewars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
