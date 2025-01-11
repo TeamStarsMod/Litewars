9b47c4d95b8724a48cdf061799ac010f8d9b4297
Neko110923 <neko110923@163.com>
2025-01-11 11:30:57 +0800

    Tips更新

--- Litewars-Plugin/src/main/java/xyz/litewars/litewars/lobby/tips/Tips.java
+++ Litewars-Plugin/src/main/java/xyz/litewars/litewars/lobby/tips/Tips.java
@@ -10,8 +10,10 @@ import xyz.litewars.litewars.utils.Utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class Tips {
@@ -19,31 +21,15 @@ public class Tips {
     private static BukkitTask task;
     private static List<String> tips;
     private static YamlConfiguration tipsYaml;
+    private static int repeatCount = 0;
+    private static int currentTipIndex = -1;
 
     public static void init() throws IOException {
+        Files.copy(Objects.requireNonNull(Tips.class.getClassLoader().getResourceAsStream("Tips.yml")), Path.of(Litewars.dataFolder + "Tips.yml"));
         File tipsFile = new File(Litewars.dataFolder, "Tips.yml");
-        if (tipsFile.createNewFile()) Litewars.logger.info("已创建Tips文件");
-
         tipsYaml = YamlConfiguration.loadConfiguration(tipsFile);
         tips = tipsYaml.getStringList("Tips");
-        if (tips == null || tips.isEmpty()) {
-            tips = new ArrayList<>();
-            tips.add("在资源上比不过你的对手? 试试合理分配队伍资源吧！");
-            tips.add("对手在战斗中总是占据上风? 试试分析并模仿他们的战斗技巧");
-            tips.add("&c认为您的对手有作弊嫌疑? 使用/report举报他们！");
-            tips.add("&a你喜欢&3Lite&ewars&a吗?");
-            tips.add("&b还有什么Tip是我能写的呢qwq...");
-            tips.add("你知道吗? 这些Tip可以在插件目录的Tips.yml下自定义！");
-            tips.add("谢谢你使用Litewars！");
-            tipsYaml.set("Tips", tips);
-
-            if (!tipsYaml.contains("UpdateSpeed")) {
-                tipsYaml.set("UpdateSpeed", 40L);
-            }
-
-            tipsYaml.save(tipsFile);
-            Litewars.logger.info("已添加默认Tips，在Tips.yml自定义！");
-        }
+        repeatCount = tipsYaml.getInt("RepeatCount");
     }
 
     private static void startTipsTask() {
@@ -55,12 +41,17 @@ public class Tips {
                     return;
                 }
 
-                if (!tips.isEmpty()) {
-                    String tip = tips.get(new Random().nextInt(tips.size()));
-                    for (Player player : RunningData.playersInLobby) {
-                        Litewars.nms.sendActionBar(player, Utils.reColor("&e" + tip));
-                    }
+                if (currentTipIndex == -1 || repeatCount == 0) {
+                    currentTipIndex = new Random().nextInt(tips.size());
+                    repeatCount = tipsYaml.getInt("RepeatCount");
                 }
+
+                String tip = tips.get(currentTipIndex);
+                for (Player player : RunningData.playersInLobby) {
+                    Litewars.nms.sendActionBar(player, Utils.reColor("&e" + tip));
+                }
+
+                repeatCount--;
             }
         }.runTaskTimer(Litewars.plugin, 0L, tipsYaml.getLong("UpdateSpeed"));
     }
@@ -77,6 +68,8 @@ public class Tips {
         if (task != null) {
             task.cancel();
             task = null;
+            currentTipIndex = -1;
+            repeatCount = 0;
         }
     }
 }
