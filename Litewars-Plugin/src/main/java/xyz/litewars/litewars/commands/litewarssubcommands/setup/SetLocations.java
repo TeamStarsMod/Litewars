eb73dc68217ce62fff03fe415210a59a1f5d893c
Neko110923 <neko110923@163.com>
2025-01-22 02:32:02 +0800

    更新

--- Litewars-Plugin/src/main/java/xyz/litewars/litewars/commands/litewarssubcommands/setup/SetLocations.java
+++ Litewars-Plugin/src/main/java/xyz/litewars/litewars/commands/litewarssubcommands/setup/SetLocations.java
@@ -4,7 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import xyz.litewars.litewars.RunningData;
-import xyz.litewars.litewars.api.arena.team.Team;
+import xyz.litewars.litewars.api.arena.Arena;
 import xyz.litewars.litewars.api.command.SubCommand;
 import xyz.litewars.litewars.commands.LitewarsCommand;
 import xyz.litewars.litewars.utils.Utils;
@@ -12,44 +12,26 @@ import xyz.litewars.litewars.utils.Utils;
 public class SetLocations extends SubCommand {
     public SetLocations(LitewarsCommand parent) {
         super(parent, "set-locations", "", "Litewars.admin", true, true,
-                "waiting");
+                "waitingLobby");
     }
 
     @Override
     public boolean execute(CommandSender sender, Command command, String s, String[] args) {
-        Player p = (Player) sender;
-        if (RunningData.playerTeamMap.containsKey(p)) {
-            if (args.length >= 1) {
-                Team team = RunningData.playerTeamMap.get(p);
-                switch (args[0]) {
-                    case "spawn" -> {
-                        team.setSpawn(p.getLocation());
-                        p.sendMessage(Utils.reColor("&a设置成功！"));
-                    }
-                    case "resources" -> {
-                        team.setIron(p.getLocation());
-                        team.setGold(p.getLocation());
-                        team.setEmerald(p.getLocation());
-                        p.sendMessage(Utils.reColor("&a设置成功！"));
-                    }
-                    case "shop" -> {
-                        team.setShop(p.getLocation());
-                        p.sendMessage(Utils.reColor("&a设置成功！"));
-                    }
-                    case "upgrade" -> {
-                        team.setUpgrade(p.getLocation());
-                        p.sendMessage(Utils.reColor("&a设置成功！"));
-                    }
-                }
+        Player player = (Player) sender;
+        Arena arena = RunningData.onSetupPlayerMap.get(player);
+        if (args.length == 0) {
+            player.sendMessage(Utils.reColor("&c需要更多参数！"));
+            return false;
+        }
+        switch (args[0].toLowerCase()) {
+            case "waitinglobby" -> {
             }
-        } else {
-            p.sendMessage(Utils.reColor("&c请先使用/EditTeam <TeamName>选择一个队伍编辑！"));
         }
         return true;
     }
 
     @Override
     public String getDescription() {
-        return "<set-team-location> 设置队伍出生点";
+        return "";
     }
 }
